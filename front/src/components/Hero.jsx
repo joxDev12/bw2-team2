f72e53d49c15af9e8b70e1afdf8c3d5b1017f5ab
Santi Andrea Emma <emma.santi.andrea@gmail.com>
2026-05-11 17:41:46 +0200

    update hero

--- front/src/components/Hero.jsx
+++ front/src/components/Hero.jsx
@@ -1,17 +1,45 @@
+
+
 function Hero() {
 
     return (
         <>
-            <section>
-                <h1>Vivi momenti che restano.</h1>
-                <p>Eventi pubblici o privati perogni interesse. Crea, organizza e partecipa</p>
+            <div className="px-4 py-5 my-5 text-center hero-section w-100">
+                <h1 className="display-5 fw-bold"> Vivi momenti <br /> che restano.
+                </h1>
 
-                <form action="">
-                    <input type="search" name="search" placeholder="Cerca eventi, artisti, luoghi..."/>
-                    <input type="text" name="localita" placeholder="Dove?"/>
-                    <input type="text" name="date" placeholder="Quanto?"/>
-                </form>
-            </section>
+                <div className="col-lg-10 mx-auto">
+                    <p className="lead mb-4"> Eventi pubblici o privati per ogni interesse. <br /> Crea, organizza e partecipa.
+                    </p>
+
+                    <form className="d-flex flex-column gap-2 flex-sm-row gap-sm-0 custom-border">
+                        <div className="form-floating flex-fill">
+                            <input className="form-control custom-rounded rounded-start-3"
+                                type="search" id="search" placeholder=" "/>
+
+                            <label htmlFor="search"> <i className="bi bi-search"></i> {" "}Cerca eventi, artisti, luoghi...</label>
+                        </div>
+
+                        <div className="form-floating flex-fill">
+                            <input className="form-control custom-rounded" type="text" id="geo" placeholder=" "/>
+
+                            <label htmlFor="geo"> <i className="bi bi-geo-alt"></i> {" "}Dove? </label>
+                        </div>
+
+                        <div className="form-floating flex-fill">
+                            <input className="form-control custom-rounded"
+                                type="text" id="date" placeholder=" "/>
+
+                            <label htmlFor="date"> <i className="bi bi-calendar-week"></i>
+                                {" "}Quando? </label>
+                        </div>
+
+                        <button className="btn btn-primary px-4 custom-rounded rounded-end-3"
+                            type="submit">Cerca</button>
+
+                    </form>
+                </div>
+            </div>
         </>
     );
 }
