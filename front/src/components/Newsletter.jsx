a2a9e341695280f86f3faab02f61972a38b7080e
Santi Andrea Emma <emma.santi.andrea@gmail.com>
2026-05-12 11:54:58 +0200

    update newsletter

---
+++ front/src/components/Newsletter.jsx
@@ -0,0 +1,27 @@
+function Newsletter() {
+
+    return (
+        <>
+            <div className="d-flex flex-column flex-md-row justify-content-center align-items-center px-4 py-4 w-100 gap-3 m-0 custom-newsletter text-center text-md-start">
+                <i class="bi bi-envelope custom-icon-newsletter"></i>
+                <div>
+                    <h4>Non perderti nulla!</h4>
+                    <p className="px-5 px-md-0">Iscriviti alla Newsletter e ricevi i migliori eventi selezionati direttamente nella tua inbox.</p>
+                </div>
+                <div className="d-flex flex-column justify-content-center gap-2">
+                    <form className="d-flex">
+                        <div className="form-floating flex-fill">
+                            <input className="form-control rounded-start-3 rounded-end-0" type="text" id="newsletter" placeholder=" " />
+
+                            <label htmlFor="nesletter">La tua email</label>
+                        </div>
+                        <button className="btn btn-primary px-4 rounded-start-0 rounded-end-3">Iscriviti</button>
+                    </form>
+                    <h6>Nessuno spam, promesso. Puoi annullare l'iscrizione in qualsiasi momento</h6>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Newsletter;
--- front/src/pages/HomePage.jsx
+++ front/src/pages/HomePage.jsx
@@ -1,13 +1,15 @@
 import Hero from '../components/Hero';
+import Newsletter from '../components/Newsletter';
 
 const HomePage = () => {
   return (
     <div>
       <Hero />
+      <Newsletter />
     </div>
 
 
-  )
+  );
 }
 
 export default HomePage
