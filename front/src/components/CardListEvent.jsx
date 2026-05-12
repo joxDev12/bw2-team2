8a1f7f2e30f536f52bf4e1e7f8093b43450aade3
joxDev12 <joxdev12@gmail.com>
2026-05-12 13:10:33 +0200

    fix card list event

--- front/src/components/CardListEvent.jsx
+++ front/src/components/CardListEvent.jsx
@@ -2,15 +2,17 @@ import { Link } from 'react-router-dom';
 
 function CardListEvent(card) {
   return (
+    <div>
     <link rel="stylesheet" href="" />
     <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="...">
+      <img src="..." class="card-img-top" alt="..." />
         <div class="card-body">
           <h5 class="card-title">{card.title}</h5>
           <p class="card-text">{card.text}</p>
         </div>
     </div>
     <link/>
+    </div>
   )
 }
 
