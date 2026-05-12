ff37f288ec926af280d52e6ece97b416c0dc6a35
Santi Andrea Emma <emma.santi.andrea@gmail.com>
2026-05-12 12:51:05 +0200

    update componet assistenza

--- front/src/pages/HomePage.jsx
+++ front/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import Hero from '../components/Hero';
+import Assistenza from '../components/Assistenza';
 import Newsletter from '../components/Newsletter';
 
 const HomePage = () => {
   return (
     <div>
       <Hero />
+      <Assistenza />
       <Newsletter />
     </div>
 
