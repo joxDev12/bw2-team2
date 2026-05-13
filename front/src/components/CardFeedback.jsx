562c8e760bbe0119f2aaa8bb14c093460302f123
Santi Andrea Emma <emma.santi.andrea@gmail.com>
2026-05-13 10:11:57 +0200

    fix feedback

--- front/src/components/CardFeedback.jsx
+++ front/src/components/CardFeedback.jsx
@@ -1,21 +1,24 @@
 import img1 from "../assets/img/1.webp";
+import img2 from "../assets/img/2.webp";
+import img3 from "../assets/img/3.webp";
+import img4 from "../assets/img/4.webp";
 
 function CardFeedback() {
     return (
         <>
+            <h4 className="text-center">Cosa dicono di noi</h4>
             <div className="col-12 col-md-6 col-lg-4">
                 <div className="feedback-card bg-danger h-100">
 
                     <p className="feedback-text">
-                        "EventHub mi ha permesso di scoprire concerti incredibili e vivere
-                        esperienze uniche nella mia città!"
+                        “Con EventHub ho scoperto tanti eventi interessanti vicino a me. Piattaforma semplice e veloce da usare.”
                     </p>
 
                     <div className="feedback-user">
                         <img src={img1} alt="Laura B." className="feedback-avatar" />
 
                         <div>
-                            <h6 className="mb-0 fw-bold">Laura B.</h6>
+                            <h6 className="mb-0 fw-bold">Giacomo S.</h6>
                             <small>Milano</small>
                         </div>
                     </div>
@@ -27,16 +30,15 @@ function CardFeedback() {
                 <div className="feedback-card bg-danger h-100">
 
                     <p className="feedback-text">
-                        "EventHub mi ha permesso di scoprire concerti incredibili e vivere
-                        esperienze uniche nella mia città!"
+                        “EventHub ha una grafica moderna e permette di trovare eventi in pochi clic.”
                     </p>
 
                     <div className="feedback-user">
-                        <img src={img1} alt="Laura B." className="feedback-avatar" />
+                        <img src={img2} alt="Marco R." className="feedback-avatar" />
 
                         <div>
-                            <h6 className="mb-0 fw-bold">Laura B.</h6>
-                            <small>Milano</small>
+                            <h6 className="mb-0 fw-bold">Giulia R.</h6>
+                            <small>Roma</small>
                         </div>
                     </div>
 
@@ -47,16 +49,34 @@ function CardFeedback() {
                 <div className="feedback-card bg-danger h-100">
 
                     <p className="feedback-text">
-                        "EventHub mi ha permesso di scoprire concerti incredibili e vivere
-                        esperienze uniche nella mia città!"
+                        “Organizzare eventi su EventHub è stato davvero intuitivo. Ottima gestione dei partecipanti.”
                     </p>
 
                     <div className="feedback-user">
-                        <img src={img1} alt="Laura B." className="feedback-avatar" />
+                        <img src={img3} alt="Giulia M." className="feedback-avatar" />
 
                         <div>
-                            <h6 className="mb-0 fw-bold">Laura B.</h6>
-                            <small>Milano</small>
+                            <h6 className="mb-0 fw-bold">Marco M.</h6>
+                            <small>Napoli</small>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="col-12 col-md-6 col-lg-4">
+                <div className="feedback-card bg-danger h-100">
+
+                    <p className="feedback-text">
+                        “Esperienza molto positiva con EventHub, sia come partecipante che come organizzatrice.”
+                    </p>
+
+                    <div className="feedback-user">
+                        <img src={img4} alt="Giulia M." className="feedback-avatar" />
+
+                        <div>
+                            <h6 className="mb-0 fw-bold">Simona B.</h6>
+                            <small>Palermo</small>
                         </div>
                     </div>
 
