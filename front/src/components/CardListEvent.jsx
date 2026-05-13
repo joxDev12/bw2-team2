1574e4db7bb2ee0f4992339b3c91fa5e116c80b9
Santi Andrea Emma <emma.santi.andrea@gmail.com>
2026-05-13 15:09:55 +0200

    cardEventList

--- front/src/components/CardListEvent.jsx
+++ front/src/components/CardListEvent.jsx
@@ -2,17 +2,16 @@ import { Link } from 'react-router-dom';
 
 function CardListEvent(card) {
   return (
-    <div>
-    <link rel="stylesheet" href="" />
-    <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">{card.title}</h5>
-          <p class="card-text">{card.text}</p>
+    <>
+      <div>
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">{card.title}</h5>
+            <p class="card-text">{card.text}</p>
+          </div>
         </div>
-    </div>
-    <link/>
-    </div>
+      </div>
+    </>
   )
 }
 
