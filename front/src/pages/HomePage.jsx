80f3f0315b7a30b97c078df629e8b2d6646ceec4
Santi Andrea Emma <emma.santi.andrea@gmail.com>
2026-05-13 10:50:26 +0200

    update component/ComeFunziona

--- front/src/pages/HomePage.jsx
+++ front/src/pages/HomePage.jsx
@@ -1,13 +1,16 @@
 import Hero from '../components/Hero';
+import ComeFunziona from '../components/ComeFunziona';
 import Feedback from '../components/Feedback';
 import Assistenza from '../components/Assistenza';
 import Newsletter from '../components/Newsletter';
+
 
 
 const HomePage = () => {
   return (
     <div>
       <Hero />
+      <ComeFunziona />
       <Feedback />
       <Assistenza />
       <Newsletter />
