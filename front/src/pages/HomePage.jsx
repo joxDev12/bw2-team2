7f76ff5f70f8130ac4f671466fec87a2d458b280
Santi Andrea Emma <emma.santi.andrea@gmail.com>
2026-05-13 11:28:50 +0200

    update bannerOrganizzatore

--- front/src/pages/HomePage.jsx
+++ front/src/pages/HomePage.jsx
@@ -2,12 +2,14 @@ import Hero from '../components/Hero';
 import Feedback from '../components/Feedback';
 import Assistenza from '../components/Assistenza';
 import Newsletter from '../components/Newsletter';
+import BannerOrganizzatore from '../components/BannerOrganizzatore';
 
 
 const HomePage = () => {
   return (
     <div>
       <Hero />
+      <BannerOrganizzatore />
       <Feedback />
       <Assistenza />
       <Newsletter />
